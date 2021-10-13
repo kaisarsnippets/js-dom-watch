8c4929e26ec244a7f7f2c971fffe9cab94f18f90
KaisarCode <kaisar@kaisarcode.com>
2021-10-13 12:08:30 -0300

    Attribute watch

--- dom-watch.js
+++ dom-watch.js
@@ -2,11 +2,13 @@
 function DOMWatch(ctx, opt) {
     ctx = ctx || document.documentElement;
     opt = opt || {
+        attributes: true,
         childList: true,
         subtree: true
     };
     var afnc = [];
     var dfnc = [];
+    var tfnc = [];
     this.added = function(sl, cb){
         cb = cb || function(){};
         afnc.push({ sl: sl, cb: cb });
@@ -15,6 +17,10 @@ function DOMWatch(ctx, opt) {
         cb = cb || function(){};
         dfnc.push({ sl: sl, cb: cb });
     };
+    this.attribute = function(sl, cb){
+        cb = cb || function(){};
+        tfnc.push({ sl: sl, cb: cb });
+    };
     this.destroy = function(){
         ob.disconnect();
     };
@@ -22,6 +28,7 @@ function DOMWatch(ctx, opt) {
         m.forEach(function(r){
             var added = r.addedNodes;
             var remov = r.removedNodes;
+            var attrb = r.attributeName;
             Array.from(added).forEach(function(el){
                 afnc.forEach(function(f){
                     var sl = f.sl;
@@ -40,6 +47,16 @@ function DOMWatch(ctx, opt) {
                     };
                 });
             });
+            if (attrb) {
+                tfnc.forEach(function(f){
+                    var el = r.target;
+                    var sl = f.sl;
+                    var cb = f.cb;
+                    if (el.matches && el.matches(sl)) {
+                        cb(el);
+                    }
+                });
+            }
         });
     });
     ob.observe(ctx, opt);
